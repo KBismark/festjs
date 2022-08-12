009cbfa27bbd20dbefb4024d7da4ccb14dceeed4
Bismark Yamoah <bismarkkwabenayamoah@gmail.com>
2022-08-12 17:10:42 +0000

    Update starter.jsx

--- scripts/starter.jsx
+++ scripts/starter.jsx
@@ -1,1 +1,13 @@
-{}
+//<
+  This is a new way of commenting you can use in fest
+  import app from "./app";
+  const app = require("./app");
+//>
+
+
+
+
+//There must be only one exports
+export default {
+  
+};
