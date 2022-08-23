79b419171e7a4b8e496ff2d5a70d42d0bf096330
Bismark Yamoah <bismarkkwabenayamoah@gmail.com>
2022-08-23 22:38:17 +0000

    Update and rename starter.jsx to example.jsx

---
+++ scripts/example.jsx
@@ -0,0 +1,13 @@
+//<
+  This is a new way of commenting you can use in your fest scripts
+  import Render from "../render";
+  const Render = require("../render");
+//>
+
+
+
+
+//There must be only one exports
+export default {
+  
+};
